9e957508f10428377bd7711d09850171dddc0954
defnot001 <marc.miller994@gmail.com>
2023-04-11 22:37:26 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import NavBar from './components/NavBar';
+
+export default function App() {
+  return (
+    <div className="dark:bg-slate-800 bg-white w-screen h-screen">
+      <NavBar />
+    </div>
+  );
+}
